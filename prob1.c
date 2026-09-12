f68b2c64aa64f9aad6599851dc774d40e17dad22
David Wilson <dwilso07@uafs.edu>
2026-09-11 19:00:29 -0500

    update prob1 and prob2

--- prob1.c
+++ prob1.c
@@ -2,12 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define  SIZE 100
+#define SIZE 100
 
-// *** Function Protoypes ***
+// *** Function Prototypes ***
 char *newString(char *str, int size);
-char showArray(char **strings, int count);
+void showArray(char **strings, int count);
+int getString(char str[], int max);
 
-int main(int argc, char *argv[]){
- 
+int main(int argc, char *argv[])
+{
+    char *strings[SIZE];
+    char input[SIZE];
+    int count = 0;
+
+   while (count < SIZE && getString(input, SIZE) > 0)
+    {  
+        strings[count] = newString(input, strlen(input) + 1);
+
+        if (strings[count] == NULL)
+        {
+            printf("strings of %d is null", count);
+            return 1;
+        }
+
+        count++;
+    }
+
+  
+    showArray(strings, count);
+
+   
+    for (int i = 0; i < count; i++)
+    {
+        free(strings[i]);
+    }
+
+    return 0;
+}
+
+int getString(char str[], int max)
+{
+    int ch = 0;
+    int i = 0;
+
+    while ((ch = getchar()) != EOF && ch != '\n')
+    {
+        if (i < max)
+        {
+            str[i] = ch;
+            i++;
+        }
+    }
+
+    str[i] = '\0';
+
+    return i;
+}
+
+char *newString(char *str, int size)
+{
+    char *newStr = malloc(size);
+
+    if (newStr == NULL)
+    {
+        return NULL;
+    }
+
+    strcpy(newStr, str);
+
+    return newStr;
+}
+
+void showArray(char **strings, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        printf("%s\n", strings[i]);
+    }
 }
--- prob2.c
+++ prob2.c
@@ -2,13 +2,102 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define  SIZE 100
+#define SIZE 100
 
-// *** Function Protoypes ***
+// *** Function Prototypes ***
 char *newString(char *str, int size);
-char showArray(char **strings);
+void showArray(char **strings, int count);
+int getString(char str[], int max);
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    char **strings;
+    char input[SIZE];
+    int count = 0;
+   
+    strings = malloc((SIZE + 1) * sizeof(char *));
+
+    if (strings == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    
+    while (count < SIZE && getString(input, SIZE) > 0)
+    {        
+        strings[count] = newString(input, strlen(input) + 1);
+
+        if (strings[count] == NULL)
+        {
+            printf("sting of %d is null.", count);           
+            for (int i = 0; i < count; i++)
+            {
+                free(strings[i]);
+            }
+
+            free(strings);
+            return 1;
+        }
+
+        count++;
+    }
+   
+    strings[count] = NULL;
+   
+    showArray(strings, count);
   
+    for (int i = 0; i < count; i++)
+    {
+        free(strings[i]);
+    }
+    
+    free(strings);
+
     return 0;
+}
+
+char *newString(char *str, int size)
+{
+    char *newStr;
+
+    newStr = malloc(size);
+
+    if (newStr == NULL)
+    {
+        return NULL;
+    }
+
+    strcpy(newStr, str);
+
+    return newStr;
+}
+
+void showArray(char **strings,  int count)
+{
+    char **current = strings;
+
+    while (*current != NULL)
+    {
+        printf("%s\n", *current);
+        current++;
+    }
+}
+
+int getString(char str[], int max)
+{
+    int ch = 0;
+    int i = 0;
+
+    while ((ch = getchar()) != EOF && ch != '\n')
+    {
+        if (i < max)
+        {
+            str[i] = ch;
+            i++;
+        }
+    }
+
+    str[i] = '\0';
+
+    return i;
 }
